0321a06dfbd5fc89a92fd2a671d1101effe0f366
Daeshawn <dsp341@gmail.com>
2021-12-23 22:16:11 -0600

    THAT MOTHA FOCKA TURNED ON I S2G

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 import json
 import requests
-from secrets import spotify_token, spotify_user_id, discoverWeeklyID, newPlaylistID
+from secrets import spotify_user_id, discoverWeeklyID, newPlaylistID
+from refreshToken import Refresh
 
 class savetoPlaylist:
     def __init__(self):
         self.userId = spotify_user_id
-        self.spotifyToken = spotify_token
+        self.spotifyToken = ""
         self.discoverWeeklyID = discoverWeeklyID
         self.basedSongs = ""
         self.newPlaylistID = newPlaylistID
 
     def getSongs(self):
-        print("Step 1: Find songs in discover weekly...")
+        print("Step 2: Find songs in discover weekly...")
         # loop through discover weekyly tracks and add to list
         query = "https://api.spotify.com/v1/playlists/{}/tracks".format(discoverWeeklyID)
 
         response = requests.get(query,headers={
             "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(spotify_token)
+            "Authorization": "Bearer {}".format(self.spotifyToken)
         })
 
         response_json = response.json()
@@ -32,15 +33,21 @@ class savetoPlaylist:
 
 
     def putSongsOnPLaylist(self):
-        print("Step 2: Adding songs to new playlist...")
+        print("Step 3: Adding songs to new playlist...")
         # add all the songs to the playlist
         query = "https://api.spotify.com/v1/playlists/{}/tracks?uris={}".format(self.newPlaylistID,self.basedSongs)
         response = requests.post(query,headers={
             "Content-Type": "application/json",
-            "Authorization": "Bearer {}".format(spotify_token)
+            "Authorization": "Bearer {}".format(self.spotifyToken)
         })
         print(response.json())
 
 
+    def callRefresh(self):
+        print("Step 1: Refreshing Spotify token")
+        refreshCall = Refresh()
+        self.spotifyToken = refreshCall.refresh()
+        self.getSongs()
+
 run = savetoPlaylist()
-run.getSongs()
+run.callRefresh()
