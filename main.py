b9cdac9daab11d69abe6fb789f10bec4154c167a
daeshawnpanko <dsp341@gmail.com>
2022-03-07 19:31:26 -0600

    Finished

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class savetoPlaylist:
         self.songsToDelete = []
         self.newPlaylistID = newPlaylistID
         self.deleteDict = {}
-        self.whoa = ''
 
     def getSongs(self):
         print("Step 4: Find songs in this week's discover weekly...")
@@ -29,7 +28,7 @@ class savetoPlaylist:
 
         for x in response_json["items"]:
             self.basedSongs += (x["track"]["uri"] + ",")
-        #remove the extra comma at the end of the string
+        # remove the extra comma at the end of the string
         self.basedSongs = self.basedSongs[:-1]
         #print(self.basedSongs)
         self.putSongsOnPLaylist()
@@ -45,7 +44,6 @@ class savetoPlaylist:
         })
 
         response_json = response.json()
-
 
         for x in response_json["items"]:
             #self.songsToDelete += (x["track"]["uri"] + ",")
@@ -80,7 +78,7 @@ class savetoPlaylist:
         self.deleteDict = {"tracks":self.songsToDelete}
         finalDump = json.dumps(self.deleteDict)
 
-
+        # Delete request for the songs that are already in the playlist
         query = "https://api.spotify.com/v1/playlists/{}/tracks".format(self.newPlaylistID)
         response = requests.delete(query, headers={
             "Content-Type": "application/json",
@@ -90,6 +88,12 @@ class savetoPlaylist:
 
         self.getSongs()
 
+# Order of operations
+# 1. callRefresh -> Refresh the spotify token
+# 2. getSongsToDelete -> Get request for the songs that are already on the playlist
+# 3. deleteLastWeek -> Delete request for the songs that are already on the playlist
+# 4. getSongs -> get the songs from the spotify created discover weekly playlist
+# 5. putSongsOnPlaylist -> add the songs that were fetched in getSongs to our discoverWeeklyPlaylist
 
 run = savetoPlaylist()
 run.callRefresh()
